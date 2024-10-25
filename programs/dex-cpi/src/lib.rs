6ef6c7717de1c157a4776bfe737f275361930bda
benjamintan10 <benjamintan.kelvin@gmail.com>
2024-10-25 15:26:09 -0700

    fix : libs

--- programs/dex-cpi/src/lib.rs
+++ programs/dex-cpi/src/lib.rs
@@ -14,18 +14,42 @@ pub mod cpi_swap_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        instructions::initialize::initialize(ctx)
+        instructions::initialize::handler(ctx)
     }
 
-    pub fn swap_ray(ctx: Context<SwapRaydium>, amount_in: u64, min_amount_out: u64) -> Result<()> {
-        instructions::swap_ray::swap_ray(ctx, amount_in, min_amount_out)
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        instructions::initialize::handler(ctx)
     }
 
-    pub fn swap_whirlpools(ctx: Context<SwapWhirlpools>, amount_in: u64, min_amount_out: u64) -> Result<()> {
-        instructions::swap_whirlpools::swap_whirlpools(ctx, amount_in, min_amount_out)
+    pub fn swap_raydium(
+        ctx: Context<SwapRaydium>,
+        amount_in: u64,
+        minimum_amount_out: u64,
+    ) -> Result<()> {
+        instructions::swap_raydium::handler(ctx, amount_in, minimum_amount_out)
+    }
+
+    pub fn swap_whirlpool(
+        ctx: Context<SwapWhirlpool>,
+        amount: u64,
+        other_amount_threshold: u64,
+        sqrt_price_limit: u128,
+        amount_specified_is_input: bool,
+        a_to_b: bool,
+    ) -> Result<()> {
+        instructions::swap_whirlpool::handler(
+            ctx,
+            amount,
+            other_amount_threshold,
+            sqrt_price_limit,
+            amount_specified_is_input,
+            a_to_b,
+        )
     }
 
     pub fn manage_whitelist(ctx: Context<ManageWhitelist>, user: Pubkey, add: bool) -> Result<()> {
-        instructions::manage_whitelist::manage_whitelist(ctx, user, add)
+        instructions::manage_whitelist::handler(ctx, user, add)
     }
 }
